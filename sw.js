2af3d1a2f1524fc072b3a995ab7882cd58b5a855
likhithadigitalsbvm15-sudo <likhithadigitalsbvm15@gmail.com>
2026-09-11 14:29:44 -0500

    Add service worker for caching and request handling
    Implement service worker with caching and fetch handling.

--- sw.js
+++ sw.js
@@ -1,3 +1,44 @@
-self.addEventListener('fetch', function(event) {
-  // Simple Service Worker
+const CACHE_NAME = 'easylife-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './home.html'
+];
+
+// Service Worker Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and Return Requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
