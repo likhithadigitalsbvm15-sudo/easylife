d4524f75dc9ab7a288b17abb20c972cf25e3f487
likhithadigitalsbvm15-sudo <likhithadigitalsbvm15@gmail.com>
2026-09-11 14:34:17 -0500

    Simplify service worker event listeners

--- sw.js
+++ sw.js
@@ -6,39 +6,14 @@ const urlsToCache = [
   './home.html'
 ];
 
-// Service Worker Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Cache and Return Requests
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Update Service Worker
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
